6e701e43af3aefe997a825d6316bfacc0ba9e18f
zcx <z4c1x0@qq.com>
2023-11-21 19:38:59 +0800

    基本模板

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,13 @@
 package com.hmdp;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
-
+    @Test
+    void fun() {
+        System.out.println("Hello World!");
+    }
 }
